76f3ad1dd56e8b5fa603ab63e8f4feaf8eda3f07
Jan Guzman <janfrancisco19@gmail.com>
2018-11-09 10:33:43 -0400

    Working with the dockerfile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, 'img')));
 
