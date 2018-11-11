ed659e63cfb0a5d6faa040faa2583d45ceb198d1
Jan Guzman <janfrancisco19@gmail.com>
2018-11-11 01:37:38 -0400

    Including debugging dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
+
+app.use(cors());
+app.use(logger('dev'));
 
 app.use(express.static(path.resolve(__dirname, 'img')));
 
